1424dc5a7729773e295cb5d2e3ee417ff6fd8f5d
metalgeekcz <metalgeek.cz@gmail.com>
2016-10-21 12:28:54 +0200

    class

--- dpp.h
+++ dpp.h
@@ -2,15 +2,25 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iterator>
+#include <algorithm>
+#include <memory.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
+#include <gsl/gsl_blas.h> //matrix multiplications
+
 // g++ -o main main.cc -lgsl -lgslcblas -lm
 
 using namespace std;
 
-void eig_decom (double data[], int dim, vector<double> & pEigVals, vector< vector<double> > & pEigVecs);
+class DPP{
+    public:
+        void eig_decom (double data[], int dim, vector<double> & pEigVals, vector< vector<double> > & pEigVecs);
 
-vector<int> sample_k(const vector<double> &pEigVals, const int K);
+        vector<int> sample_k(const vector<double> &pEigVals, const int K);
 
-vector<int> sample_dpp(const vector<double> &pEigVals, const vector<vector<double> > & pEigVecs, const int K);
-
+        vector<int> sample_dpp(const vector<double> &pEigVals, const vector<vector<double> > & pEigVecs, const int K);
+    
+};
--- main.cc
+++ main.cc
@@ -2,24 +2,29 @@
 
 int main (void)
 {
- /* double data[] = { -1.0, 1.0, -1.0, 1.0,
-                    -8.0, 4.0, -2.0, 1.0,
-                    27.0, 9.0, 3.0, 1.0,
-                    64.0, 16.0, 4.0, 1.0 };*/
-   double data[] ={  5.0 ,    7.0  ,  10.0  ,   5.0  ,   9.0,
-                     7.0 ,   26.0  ,  14.0  ,   7.0  ,  27.0,
-                    10.0 ,   14.0  ,  20.0  ,  10.0  ,  18.0,
-                     5.0 ,    7.0  ,  10.0  ,   5.0  ,   9.0,
-                     9.0 ,   27.0  ,  18.0  ,   9.0  ,  29.0};
-  int dim=5;
-  
+  //the data is the similarity measure matrix which can be obtained such as B*B' where B is doc*feature matrix
+   double data[] ={    
+     
+    5.2100,    5.3000,    5.2000,   12.2110,   12.2550,   12.3000,    9.5550,    9.5110,    9.5550,
+    5.3000,    5.4100,    5.3050,   12.5100,   12.5500,   12.6050,    9.2500,    9.2100,    9.2500,
+    5.2000,    5.3050,    5.2025,   12.2600,   12.3000,   12.3525,    9.1500,    9.1100,    9.1500,
+   12.2110,   12.5100,   12.2600,   29.0401,   29.1205,   29.2700,   20.1505,   20.0701,   20.1505,
+   12.2550,   12.5500,   12.3000,   29.1205,   29.2025,   29.3500,   20.3525,   20.2705,   20.3525,
+   12.3000,   12.6050,   12.3525,   29.2700,   29.3500,   29.5025,   20.2000,   20.1200,   20.2000,
+    9.5550,    9.2500,    9.1500,   20.1505,   20.3525,   20.2000,   29.5025,   29.3005,   29.5025,
+    9.5110,    9.2100,    9.1100,   20.0701,   20.2705,   20.1200,   29.3005,   29.1001,   29.3005,
+    9.5550,    9.2500,    9.1500,   20.1505,   20.3525,   20.2000,   29.5025,   29.3005,   29.5025
+   };
+  int dim=9;
+  int K=3;
+  DPP myDPP; 
   vector<double> lEigVals;
   vector<vector<double> > lEigVecs;
-  eig_decom (data, dim, lEigVals, lEigVecs);
+  myDPP.eig_decom (data, dim, lEigVals, lEigVecs);
+
   //sampling from a DPP
-  int K=3;
   vector<int> Sample_points;
-  Sample_points=sample_dpp(lEigVals, lEigVecs, K);
+  Sample_points=myDPP.sample_dpp(lEigVals, lEigVecs, K);
 
   cout<<"Sample_points \n";
   for (int ss=0; ss<Sample_points.size(); ss++){
